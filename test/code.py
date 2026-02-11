d3752f9de5454a2632d28c0b52a8bd18d38cb8d6
artail2020 <artail2012@gmail.com>
2026-02-11 14:29:02 +0900

    関数の基本

--- test/code.py
+++ test/code.py
@@ -1,3 +1,7 @@
-age = 19
-result = (not (age >= 20))
-print(result)
+def question_exclamation_text(text1: str, text2: str):
+    result1 = text1 + "?"
+    result2 = text2 + "!"
+    return result1, result2
+
+r1, r2 = question_exclamation_text("apple", "banana")
+print(r1 + " " + r2)
