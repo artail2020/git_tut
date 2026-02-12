63c640ed05524f246b8693e172213a2088dcb578
artail2020 <artail2012@gmail.com>
2026-02-12 16:55:43 +0900

    クラスの改造

--- test/code.py
+++ test/code.py
@@ -1,12 +1,14 @@
+from decimal import Decimal
+
 class BodyCond:
-    def __init__(self, arg_height: int, arg_weight: int):
-        self.height = arg_height
-        self.weight = arg_weight
-        
+    def __init__(self, arg_height: Decimal, arg_weight: Decimal):
+        self.height: Decimal = Decimal(arg_height)
+        self.weight: Decimal = Decimal(arg_weight)
+                
     def bmi_calc(self):
-        m_height: float = self.height / 100
-        bmi: float = self.weight / (m_height ** 2)
+        m_height: Decimal = Decimal(self.height / 100)
+        bmi: Decimal = Decimal(self.weight / (m_height ** 2))
         print(str(round(bmi, 2)))
 
-bc = BodyCond(168, 70)
+bc = BodyCond(Decimal(168.0), Decimal(69.9))
 bc.bmi_calc()
