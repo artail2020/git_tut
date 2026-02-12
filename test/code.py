792de521e0bb7250171df3f3702af3d15ed7bceb
artail2020 <artail2012@gmail.com>
2026-02-12 16:49:43 +0900

    クラスの基本

--- test/code.py
+++ test/code.py
@@ -1,7 +1,12 @@
-def question_exclamation_text(text1: str, text2: str):
-    result1 = text1 + "?"
-    result2 = text2 + "!"
-    return result1, result2
+class BodyCond:
+    def __init__(self, arg_height: int, arg_weight: int):
+        self.height = arg_height
+        self.weight = arg_weight
+        
+    def bmi_calc(self):
+        m_height: float = self.height / 100
+        bmi: float = self.weight / (m_height ** 2)
+        print(str(round(bmi, 2)))
 
-r1, r2 = question_exclamation_text("apple", "banana")
-print(r1 + " " + r2)
+bc = BodyCond(168, 70)
+bc.bmi_calc()
