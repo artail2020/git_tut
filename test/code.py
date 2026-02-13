2c2c89c9c45ec58ab3b517223735f0968ba3efd8
artail2020 <artail2012@gmail.com>
2026-02-13 16:39:38 +0900

    例外処理

--- test/code.py
+++ test/code.py
@@ -1,14 +1,7 @@
-from decimal import Decimal
+x = 1
+y = 0
 
-class BodyCond:
-    def __init__(self, arg_height: Decimal, arg_weight: Decimal):
-        self.height: Decimal = arg_height
-        self.weight: Decimal = arg_weight
-                
-    def bmi_calc(self):
-        m_height: Decimal = self.height / 100
-        bmi: Decimal = self.weight / (m_height ** 2)
-        print(str(round(bmi, 2)))
-
-bc = BodyCond(Decimal("168.0"), Decimal("69.9"))
-bc.bmi_calc()
+try:
+    result = x / y
+except Exception as e:
+    print("エラー " + str(e))
