df4d1c0c0d36d134eab02b17b4c43bcb937c8397
artail2020 <artail2012@gmail.com>
2026-03-02 16:06:32 +0900

    クラスの継承

--- test/code.py
+++ test/code.py
@@ -1,3 +1,21 @@
-age: float = 0.5
-result: str = "成人" if age >= 20 else "子供" if age >= 1 else "赤ちゃん"
-print(result)
+class WizardClass:
+    def __init__(self) -> None:
+        print("WizardClass init")
+        self.mp: int = 50
+ 
+    def cast_spell(self) -> None:
+        print("呪文詠唱")
+
+class SwordmanClass:
+    def __init__(self) -> None:
+        print("SwordmanClass init")
+    
+    def attack_sword(self) -> None:
+        print("剣で攻撃")
+
+class MagicSwordmanClass(WizardClass, SwordmanClass):
+    pass
+
+msmc: MagicSwordmanClass = MagicSwordmanClass()
+msmc.cast_spell()
+msmc.attack_sword()
