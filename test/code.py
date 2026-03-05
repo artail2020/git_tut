f4af5efcedb06fbaf38474fbab4b7649ca4767a8
artail2020 <artail2012@gmail.com>
2026-03-05 16:57:21 +0900

    タイプアノテーション

--- test/code.py
+++ test/code.py
@@ -1,3 +1,6 @@
-kanto: list[str] = ["千葉", "栃木", "東京", "埼玉", "茨城", "群馬", "神奈川"]
-x: list[str] = [f"{i}都" if (i == "東京") else f"{i}県" for i in kanto]
-print(x)
+def total_price_1item(unit_price: int, quantity: int = 1) -> str:
+    total_price: int = unit_price * quantity
+    return f'\\{total_price:,}'
+
+total: str = total_price_1item(unit_price=1300)
+print(total)
