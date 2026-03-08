0166aba58617fe4bc068fa3df448a0eff8726b05
artail2020 <artail2012@gmail.com>
2026-03-08 16:18:19 +0900

    ログ出力

--- test/code.py
+++ test/code.py
@@ -1,3 +1,26 @@
-num: list[int] = [5, 8, 10, 12, 30]
-result: list[int] = list(filter(lambda x: x >= 10, num))
-print(result)
+from logging import getLogger, FileHandler, DEBUG, ERROR, Formatter
+
+# formatter,logger設定
+formatter = Formatter("[%(levelname)s] %(asctime)s - %(message)s (%(filename)s)")
+logger = getLogger(__name__)
+
+# DEBUGハンドラー設定
+handler = FileHandler("./logs/log.txt", encoding="utf-8")
+handler.setLevel(DEBUG)
+handler.setFormatter(formatter)
+
+# ERRORハンドラー設定
+err_handler = FileHandler("./logs/error.txt", encoding="utf-8")
+err_handler.setLevel(ERROR)
+err_handler.setFormatter(formatter)
+
+# loggerにハンドラーを追加
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.addHandler(err_handler)
+
+# ログ出力
+logger.info("プログラムが開始されました")
+logger.debug("入力値は1000です")
+logger.warning("ファイルの容量が200GBを超えました")
+logger.error("ファイルが存在していません")
